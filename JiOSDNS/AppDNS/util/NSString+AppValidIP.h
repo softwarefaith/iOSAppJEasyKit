5c7bb8b707735af1c6b33e75fcc4b82a02f646e0
蔡杰 <jie.cai@mljr.com>
2017-09-21 16:30:01 +0800

    DNS

---
+++ JiOSDNS/AppDNS/util/NSString+AppValidIP.h
@@ -0,0 +1,13 @@
+//
+//  NSString+AppValidIP.h
+//  JiOSDNS
+//
+//  Created by 蔡杰 on 2017/9/6.
+//  Copyright © 2017年 蔡杰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (AppValidIP)
+
+@end
