1c82c6c511471676ea56253a909b1ee1070bb73d
蔡杰 <jie.cai@mljr.com>
2017-12-28 17:43:23 +0800

    建立 MVP   MVVM工程

---
+++ MVP-Objc/MVP-Objc/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MVP-Objc
+//
+//  Created by 蔡杰 on 2017/12/28.
+//  Copyright © 2017年 蔡杰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ MVVM-Objc/MVVM-Objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVM-Objc
+//
+//  Created by 蔡杰 on 2017/12/28.
+//  Copyright © 2017年 蔡杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
